875c8c6bedb3011db23e60b85b721a54912ba5cf
Squaad6z <carneiromoreirasteven22@gmail.com>
2023-05-16 16:30:16 +0200

    EventListener flèche gauche + changement image

--- assets/script.js
+++ assets/script.js
@@ -19,23 +19,38 @@ const slides = [
 
 
 const arrowRight = document.querySelector(".arrow_right");
+const arrowLeft = document.querySelector(".arrow_left");
 const currentBanner = document.querySelector(".banner-img");
 
-const currentPath = './assets/images/slideshow/';
-let cpt =0;
+const currentPath = './assets/images/slideshow/';//chemin d'accès des images
+let cpt = 0;// Initialiser compteur à 0
 
-const init = ()=> {
-	currentBanner.src= currentPath +slides[cpt].image;
-}
-arrowRight.addEventListener('click',()=>{
-	if(cpt == slides.length - 1){ // si on est sur la dernière diapositive
-		cpt = 0; // retourner à la première diapositive
-	}else{
-		cpt++;	
-	}	
-	currentBanner.src= currentPath + slides[cpt].image;
+const init = () => { // Fonction pour afficher la première diapositive
+  currentBanner.src = currentPath + slides[cpt].image;
+};
 
-    console.log("flèche de droite");
+
+arrowRight.addEventListener('click', () => {// écouteur d'événements diapositive suivante
+  if (cpt == slides.length - 1) { //si on est sur la dernière diapositive
+    cpt = 0; // retourner à la première diapositive
+  } else {
+    cpt++; // passer à la diapositive suivante
+  }
+  currentBanner.src = currentPath + slides[cpt].image; // Changement image
+
+  console.log("flèche de droite");
 });
+
+arrowLeft.addEventListener('click', () => {
+  if (cpt == 0) {// si on est sur la première diapositive
+    cpt = slides.length - 1; // retourner à la dernière diapositive
+  } else {
+    cpt--; 
+  }
+  currentBanner.src = currentPath + slides[cpt].image;
+
+  console.log("flèche de gauche");
+});
+
 
 init();
