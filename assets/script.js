62d0cdee6b2754f85b9cf50d1ea2534f00df2f88
Squaad6z <carneiromoreirasteven22@gmail.com>
2023-05-16 15:46:35 +0200

    EventListener flèche droit + changement image

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,26 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+const arrowRight = document.querySelector(".arrow_right");
+const currentBanner = document.querySelector(".banner-img");
+
+const currentPath = './assets/images/slideshow/';
+let cpt =0;
+
+const init = ()=> {
+	currentBanner.src= currentPath +slides[cpt].image;
+}
+arrowRight.addEventListener('click',()=>{
+	if(cpt == slides.length - 1){ // si on est sur la dernière diapositive
+		cpt = 0; // retourner à la première diapositive
+	}else{
+		cpt++;	
+	}	
+	currentBanner.src= currentPath + slides[cpt].image;
+
+    console.log("flèche de droite");
+});
+
+init();
