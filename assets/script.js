57ffc4860fdec440d8d7e8e6a2fda40d35f98bb8
Squaad6z <carneiromoreirasteven22@gmail.com>
2023-05-16 17:55:59 +0200

    Ajout Bullet Point + dot_selected

--- assets/script.js
+++ assets/script.js
@@ -21,14 +21,30 @@ const slides = [
 const arrowRight = document.querySelector(".arrow_right");
 const arrowLeft = document.querySelector(".arrow_left");
 const currentBanner = document.querySelector(".banner-img");
+const dots = document.querySelector(".dots");
 
 const currentPath = './assets/images/slideshow/';//chemin d'accès des images
 let cpt = 0;// Initialiser compteur à 0
 
 const init = () => { // Fonction pour afficher la première diapositive
   currentBanner.src = currentPath + slides[cpt].image;
-};
 
+for(let i =0; i < slides.length;i++){ //Pour chaque elements
+    const span = document.createElement('span'); //Crée balise span
+    span.classList = "dot"; //class .dot
+    dots.appendChild(span); //envoie parents
+    }
+    activeDot()
+
+};
+const activeDot = () => {
+    const tabDots = document.querySelectorAll(".dot"); // sélectionne tous les points
+    for (let i = 0; i < tabDots.length; i++) { 
+        //boucle pour enlever "dot_selected" des points
+      tabDots[i].classList = "dot"; // enlève "dot_selected"
+    }
+    tabDots[cpt].classList = "dot dot_selected"; // ajoute "dot_selected"
+}
 
 arrowRight.addEventListener('click', () => {// écouteur d'événements diapositive suivante
   if (cpt == slides.length - 1) { //si on est sur la dernière diapositive
@@ -37,7 +53,7 @@ arrowRight.addEventListener('click', () => {// écouteur d'événements diaposit
     cpt++; // passer à la diapositive suivante
   }
   currentBanner.src = currentPath + slides[cpt].image; // Changement image
-
+  activeDot()
   console.log("flèche de droite");
 });
 
@@ -48,7 +64,7 @@ arrowLeft.addEventListener('click', () => {
     cpt--; 
   }
   currentBanner.src = currentPath + slides[cpt].image;
-
+  activeDot()
   console.log("flèche de gauche");
 });
 
